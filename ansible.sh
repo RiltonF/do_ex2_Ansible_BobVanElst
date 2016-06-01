bb87a258bf6753957dbef54a5058aba90df88b0e
Maarten Luyts <luyts.m@gmail.com>
2016-06-01 13:28:07 +0200

    update vagrant provisioner

--- ansible.sh
+++ ansible.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
+apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible sshpass
 
 ln -sf /vagrant/hosts /etc/ansible/hosts
 
-rm /home/vagrant/.ssh/known_host
 
 KNOW_HOSTS_FILE="/home/vagrant/.ssh/known_hosts"
 
-ssh-keyscan 192.168.2.2 >> $KNOW_HOSTS_FILE 
+[ -f "$KNOW_HOSTS_FILE" ] && rm $KNOW_HOSTS_FILE 
+
+ssh-keyscan 192.168.2.3 >> $KNOW_HOSTS_FILE 
 ssh-keyscan 192.168.2.4 >> $KNOW_HOSTS_FILE
-ssh-keyscan localhost >> $KNOW_HOSTS_FILE 
+ssh-keyscan 192.168.2.5 >> $KNOW_HOSTS_FILE
+ssh-keyscan 192.168.2.6 >> $KNOW_HOSTS_FILE
 chown vagrant:vagrant $KNOW_HOSTS_FILE 
+
+# TODO: ssh-keyscan can be replaced if a playbook is created that copies the
+# files, ssh-agent needs to be implemented as wel.
 
